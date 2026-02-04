c4ba20ba3c83d7eceffc618c3e068e00ef2f84c0
glsaikiran <geniusglsai@gmail.com>
2026-02-04 11:35:21 +0530

    Production config complete

--- kirviko/settings.py
+++ kirviko/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os  # Add at top if missing
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n&=rd+k_*94=&b+gmu4_(%5&#ix+i8fhc)+sjtj$@hq0_@fz9g'
+SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False   # Production security
