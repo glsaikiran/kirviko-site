7c20e114d3b4f1e9e26631b2dce1524b18aa35e6
glsaikiran <geniusglsai@gmail.com>
2026-04-28 17:04:38 +0530

    Admin page updated

--- aquarium/admin.py
+++ aquarium/admin.py
@@ -1,9 +1,16 @@
-
-
-# Register your models here.
 from django.contrib import admin
 from .models import Product
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'stock']
+    list_display = ['name', 'rupee_price', 'stock', 'image']
+    list_editable = ['stock', 'image']          # edit stock & image right from the list page
+    search_fields = ['name']
+    ordering = ['name']
+
+    fields = ['name', 'price', 'image', 'stock'] # controls what shows in the edit form
+
+    def rupee_price(self, obj):
+        return f"₹{obj.price // 100}"
+    rupee_price.short_description = 'Price (₹)'
